9a20e1b4e40c9b0c712dae07d7a48790a57c8d5b
carlnathF19 <54322367+carlnathF19@users.noreply.github.com>
2025-03-23 14:49:04 -0700

    Error Error Will Robinson

---
+++ MyMod/TMH/IGrenadeCardController.cs
@@ -0,0 +1,72 @@
+﻿// SentinelsEngine, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// Handelabra.Sentinels.Engine.Controller.LanternJack.iGrenadeCardController
+using System;
+using System.Collections;
+using Handelabra.Sentinels.Engine.Controller;
+//Needs to be updated to TMH location using Handelabra.Sentinels.Engine.Controller.LanternJack; 
+using Handelabra.Sentinels.Engine.Model;
+
+namespace ISSUEComics.TMH
+{
+	public class IGrenadeCardController : TMHOneShotCardController
+	{
+		public IGrenadeCardController(Card card, TurnTakerController turnTakerController)
+			: base(card, turnTakerController)
+		{
+			base.SpecialStringMaker.ShowSpecialString(() => "The difference between The Machine Heart's current and maximum HP is " + GetXDamage() + ".");
+		}
+
+		protected override IEnumerator OneShotEffect()
+		{
+			GameController gameController = base.GameController;
+			HeroTurnTakerController decisionMaker = DecisionMaker;
+			DamageSource source = new DamageSource(base.GameController, base.CharacterCard);
+			int xDamage = GetXDamage();
+			int? numberOfTargets = 1;
+			int? requiredTargets = 1;
+			CardSource cardSource = GetCardSource();
+
+			IEnumerator coroutine = GameController.SetHP(base.CharacterCard, 13, [CardSource = cardSource = null]);
+			if (base.UseUnityCoroutines)
+			{
+				yield return base.GameController.StartCoroutine(coroutine);
+			}
+			else
+			{
+				base.GameController.ExhaustCoroutine(coroutine);
+			}
+
+			IEnumerator coroutine2 = base.GameController.SelectTargetsAndDealDamage(decisionMaker, source, xDamage, DamageType.Radiant, numberOfTargets, optional: false, requiredTargets, isIrreducible: true, allowAutoDecide: false, autoDecide: false, null, null, null, null, null, selectTargetsEvenIfCannotDealDamage: false, null, null, ignoreBattleZone: false, null, cardSource);
+			if (base.UseUnityCoroutines)
+			{
+				yield return base.GameController.StartCoroutine(coroutine2);
+			}
+			else
+			{
+				base.GameController.ExhaustCoroutine(coroutine2);
+			}			
+
+			IEnumerator coroutine3 = base.GameController.ShuffleCardIntoLocation(DecisionMaker, base.Card, base.TurnTaker.Deck, optional: false, toBottom: false, GetCardSource());
+			if (base.UseUnityCoroutines)
+			{
+				yield return base.GameController.StartCoroutine(coroutine3);
+			}
+			else
+			{
+				base.GameController.ExhaustCoroutine(coroutine3);
+			}
+		}
+
+		private int GetXDamage()
+		{
+			if (base.CharacterCard.HitPoints.Value > 13)
+			{
+				return Math.Abs(13 - base.CharacterCard.HitPoints.Value);
+			}
+			else
+			{
+				return 0;
+			}
+		}
+	}
+}
